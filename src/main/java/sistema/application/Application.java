2d13477ec45a10d0cb0107a7a925127d724a65f4
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-09-07 17:43:54 -0600

    Actualización de clase Application y package principal

--- src/main/java/sistema/application/Application.java
+++ src/main/java/sistema/application/Application.java
@@ -1,7 +1,22 @@
 package sistema.application;
 
+import sistema.presentation.principal.View;
+
+import javax.swing.*;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e)
+        {e.printStackTrace();}
+
+        View view = new View();
+
+        JFrame windows = new JFrame("SISE: Sistema de Sucursales y Empleados");
+        windows.setSize(600, 400);
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.setContentPane(view.getJPanel());
+        windows.setVisible(true);
     }
 }
--- src/main/java/sistema/presentation/principal/View.java
+++ src/main/java/sistema/presentation/principal/View.java
@@ -1,4 +1,55 @@
 package sistema.presentation.principal;
 
-public class View {
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class View extends JFrame {
+    private JTabbedPane tabbedPane;
+    private JPanel panel1;
+    private JLabel refenciaLbl;
+    private JTextField referenciaTxtField;
+    private JButton buscarReferenciaButton;
+    private JButton agregarButton;
+    private JButton borrarButton;
+    private JButton reporteButton;
+    private JTable table1;
+
+    public JPanel getJPanel(){
+        return panel1;
+    }
+
+    public View() {
+//        setIconImage(new ImageIcon(getClass().getResource("/imagenes/icono-empleados.png")).getImage());
+        buscarReferenciaButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String texto = referenciaTxtField.getText();
+                texto=texto.replaceAll(" ", "");
+                if (texto.length() != 0) {
+                    JOptionPane.showMessageDialog(null, "Buscando...");
+                }else{
+                    JOptionPane.showMessageDialog(null, "¡El campo no puede estar vacio!", "Aviso",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
+        referenciaTxtField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                int key = e.getKeyChar();
+                boolean mayusculas = key >= 65 && key <= 90;
+                boolean minusculas = key >= 97 && key <= 122;
+                boolean espacio = key == 32;
+
+                if (!(minusculas || mayusculas || espacio))
+                {
+                    e.consume();
+                }
+            }
+        });
+    }
 }

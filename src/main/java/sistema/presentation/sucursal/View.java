b7f1fdab1fdd2d589878d702523dbf2287302755
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-09-08 11:00:33 -0600

    Actualización de package sucursal

--- src/main/java/sistema/presentation/sucursal/View.java
+++ src/main/java/sistema/presentation/sucursal/View.java
@@ -1,6 +1,10 @@
 package sistema.presentation.sucursal;
 
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Observable;
 import sistema.logic.Sucursal;
 
@@ -9,11 +13,60 @@ import javax.swing.*;
 
 public class View extends javax.swing.JFrame implements java.util.Observer {
     private JPanel panel1;
+    private JTextField codigoSucursalTxt;
+    private JTextField referenciaSucursalTxt;
+    private JTextField direccionSucursalTxt;
+    private JTextField zonajeSucursalTxt;
+    private JButton guardarSucursalBtn;
+    private JButton cancelarSucursalBtn;
 
     Controller controller;
     Model model;
     public View() {
 
+        cancelarSucursalBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                controller.hide();
+            }
+        });
+        guardarSucursalBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String campoCodigo = codigoSucursalTxt.getText();
+                String campoReferencia = referenciaSucursalTxt.getText();
+                String campoDireccion = direccionSucursalTxt.getText();
+                String campoZonaje = zonajeSucursalTxt.getText();
+                campoCodigo = campoCodigo.replaceAll(" ", "");
+                campoReferencia = campoReferencia.replaceAll(" ", "");
+                campoDireccion = campoDireccion.replaceAll(" ", "");
+                campoZonaje = campoZonaje.replaceAll(" ", "");
+                if (campoCodigo.length() != 0 && campoReferencia.length() != 0 && campoDireccion.length() != 0 && campoZonaje.length() != 0) {
+                    int value = JOptionPane.showConfirmDialog(null, "¿Desea guardar los cambios?");
+                    if (JOptionPane.OK_OPTION == value) {
+                        JOptionPane.showMessageDialog(null, "Guardado con exito");
+                        controller.hide();
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "¡Los campos no pueden estar vacios!", "Aviso",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        referenciaSucursalTxt.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                int key = e.getKeyChar();
+                boolean mayusculas = key >= 65 && key <= 90;
+                boolean minusculas = key >= 97 && key <= 122;
+                boolean espacio = key == 32;
+
+                if (!(minusculas || mayusculas || espacio))
+                {
+                    e.consume();
+                }
+            }
+        });
     }
 
     public void setController(Controller controller){

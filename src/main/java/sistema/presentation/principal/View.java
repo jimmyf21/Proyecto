c20e741bc0eb036fcb73c5e98c3d5eed70565b76
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-09-07 20:44:45 -0600

    Actualizacion de package principal

--- src/main/java/sistema/presentation/principal/View.java
+++ src/main/java/sistema/presentation/principal/View.java
@@ -12,10 +12,14 @@ public class View extends JFrame {
     private JLabel refenciaLbl;
     private JTextField referenciaTxtField;
     private JButton buscarReferenciaButton;
+    private JTextField nombreTxt;
+    private JButton buscarButton;
+    private JButton agregarButton1;
+    private JButton borrarButton1;
+    private JButton reporteButton1;
     private JButton agregarButton;
     private JButton borrarButton;
     private JButton reporteButton;
-    private JTable table1;
 
     public JPanel getJPanel(){
         return panel1;
@@ -48,6 +52,33 @@ public class View extends JFrame {
                 if (!(minusculas || mayusculas || espacio))
                 {
                     e.consume();
+                }
+            }
+        });
+        nombreTxt.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                int key = e.getKeyChar();
+                boolean mayusculas = key >= 65 && key <= 90;
+                boolean minusculas = key >= 97 && key <= 122;
+                boolean espacio = key == 32;
+
+                if (!(minusculas || mayusculas || espacio))
+                {
+                    e.consume();
+                }
+            }
+        });
+        buscarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String texto = referenciaTxtField.getText();
+                texto=texto.replaceAll(" ", "");
+                if (texto.length() != 0) {
+                    JOptionPane.showMessageDialog(null, "Buscando...");
+                }else{
+                    JOptionPane.showMessageDialog(null, "¡El campo no puede estar vacio!", "Aviso",
+                            JOptionPane.WARNING_MESSAGE);
                 }
             }
         });
